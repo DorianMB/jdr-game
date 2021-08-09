e4ee544c4f4402d995d2b17c701d53090c29f81b
Dorian <dorian.marques.2e7@gmail.com>
2021-08-10 01:25:31 +0200

    add ApiUrls

--- components/Test.vue
+++ components/Test.vue
@@ -7,24 +7,24 @@
 
 <script>
 import {User} from "../static/models/user";
-import {getNameAndEmail} from "../static/Functions";
-import {Bag} from "../static/models/bag";
-import {LootTable} from "../static/models/lootTable";
 import {Item} from "../static/models/item";
+
+import {getNameAndEmail} from "../static/Functions";
+import * as ApiUrls from "../static/ApiUrls";
 
 export default {
   name: "Test",
   data() {
     return {
       user: User,
-      lootTable: [Item]
+      items: [Item]
     }
   },
   async beforeMount() {
     const sendUser = {mail: 'test@test.fr', password: 'test'};
-    this.user = await this.$axios.$post('http://localhost:3000/api/connection', sendUser);
-    this.lootTable = await this.$axios.$get('http://localhost:3000/api/items');
-    console.log(this.user, this.lootTable);
+    this.user = await this.$axios.$post(ApiUrls.GET_API_CONNECTION(), sendUser);
+    this.items = await this.$axios.$get(ApiUrls.GET_API_ALL_ITEMS());
+    console.log(this.user, this.items);
   },
   methods: {
     getNameAndEmail,
---
+++ static/ApiUrls.ts
@@ -0,0 +1,65 @@
+const _BASE_API = "http://localhost:3000/api";
+const _CONNECTION = "/connection";
+const _USERS = "/users";
+const _CARACTERS = "/caracters";
+const _EQUIPEMENTS = "/equipements";
+const _BAGS = "/bags";
+const _STATS = "/stats";
+const _LOOT_TABLE = "/loot-table";
+const _ITEMS = "/items";
+
+/**
+ * @return {string}
+ */
+export const GET_API_CONNECTION = () => {
+  return `${_BASE_API}${_CONNECTION}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_USERS = () => {
+  return `${_BASE_API}${_USERS}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_CARACTERS = () => {
+  return `${_BASE_API}${_CARACTERS}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_EQUIPEMENTS = () => {
+  return `${_BASE_API}${_EQUIPEMENTS}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_BAGS = () => {
+  return `${_BASE_API}${_BAGS}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_STATS = () => {
+  return `${_BASE_API}${_STATS}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_LOOT_TABLE = () => {
+  return `${_BASE_API}${_LOOT_TABLE}`;
+};
+
+/**
+ * @return {string}
+ */
+export const GET_API_ALL_ITEMS = () => {
+  return `${_BASE_API}${_ITEMS}`;
+};
