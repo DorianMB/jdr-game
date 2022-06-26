0e04aa46cc4763978b0a0e5b11ae8b2f0e59f7e0
Dorian <dorian.marques.2e7@gmail.com>
2022-06-26 14:35:29 +0200

    add item tooltip

--- pages/admin/caracters/_id.vue
+++ pages/admin/caracters/_id.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="w-100 p-5 row">
+  <div class="w-100 p-5 row" @click="closeTooltip()">
     <div class="col-3 d-flex flex-wrap justify-content-center">
-      <div class="item-slot" id="helmet-slot">
+      <div class="item-slot" id="helmet-slot" @mouseover="openTooltip($event, itemsInEquipement.helmet)">
         <img v-if="itemsInEquipement.helmet && itemsInEquipement.helmet.loot_id"
              :id="getTypeByLootTable(itemsInEquipement.helmet.loot_id) + itemsInEquipement.helmet.item_id"
              :src="getImageByLootTable(itemsInEquipement.helmet.loot_id)" width="100" height="100"
              data-type="helmet">
       </div>
-      <div class="item-slot" id="chestplate-slot">
+      <div class="item-slot" id="chestplate-slot" @mouseover="openTooltip($event, itemsInEquipement.chestplate)">
         <img v-if="itemsInEquipement.chestplate && itemsInEquipement.chestplate.loot_id"
              :id="getTypeByLootTable(itemsInEquipement.chestplate.loot_id) + itemsInEquipement.chestplate.item_id"
              :src="getImageByLootTable(itemsInEquipement.chestplate.loot_id)" width="100" height="100"
              data-type="chestplate">
       </div>
-      <div class="item-slot" id="primary-magic-item-slot">
+      <div class="item-slot" id="primary-magic-item-slot" @mouseover="openTooltip($event, itemsInEquipement.primary_magic_item)">
         <img v-if="itemsInEquipement.primary_magic_item && itemsInEquipement.primary_magic_item.loot_id"
              :id="getTypeByLootTable(itemsInEquipement.primary_magic_item.loot_id) + itemsInEquipement.primary_magic_item.item_id"
              :src="getImageByLootTable(itemsInEquipement.primary_magic_item.loot_id)" width="100" height="100"
@@ -22,18 +22,33 @@
     </div>
     <div class="col-6 d-flex flex-wrap justify-content-center">
       <div class="img-caracter" :style="{ backgroundImage: `url(${caracter.picture})`}"></div>
-      <div class="item-slot mx-2" id="primary-weapon-slot">
+      <div class="item-slot mx-2" id="primary-weapon-slot" @mouseover="openTooltip($event, itemsInEquipement.primary_weapon)">
         <img v-if="itemsInEquipement.primary_weapon && itemsInEquipement.primary_weapon.loot_id"
              :id="getTypeByLootTable(itemsInEquipement.primary_weapon.loot_id) + itemsInEquipement.primary_weapon.item_id"
              :src="getImageByLootTable(itemsInEquipement.primary_weapon.loot_id)" width="100" height="100"
              data-type="primary_weapon">
       </div>
-      <div class="item-slot mx-2" id="secondary-weapon-slot"></div>
+      <div class="item-slot mx-2" id="secondary-weapon-slot" @mouseover="openTooltip($event, itemsInEquipement.secondary_weapon)">
+        <img v-if="itemsInEquipement.secondary_weapon && itemsInEquipement.secondary_weapon.loot_id"
+             :id="getTypeByLootTable(itemsInEquipement.secondary_weapon.loot_id) + itemsInEquipement.secondary_weapon.item_id"
+             :src="getImageByLootTable(itemsInEquipement.secondary_weapon.loot_id)" width="100" height="100"
+             data-type="secondary_weapon">
+      </div>
     </div>
     <div class="col-3 d-flex flex-wrap justify-content-center">
-      <div class="item-slot" id="gloves-slot"></div>
-      <div class="item-slot" id="boots-slot"></div>
-      <div class="item-slot" id="secondary-magic-item-slot">
+      <div class="item-slot" id="gloves-slot" @mouseover="openTooltip($event, itemsInEquipement.gloves)">
+        <img v-if="itemsInEquipement.gloves && itemsInEquipement.gloves.loot_id"
+             :id="getTypeByLootTable(itemsInEquipement.gloves.loot_id) + itemsInEquipement.gloves.item_id"
+             :src="getImageByLootTable(itemsInEquipement.gloves.loot_id)" width="100" height="100"
+             data-type="gloves">
+      </div>
+      <div class="item-slot" id="boots-slot" @mouseover="openTooltip($event, itemsInEquipement.boots)">
+        <img v-if="itemsInEquipement.boots && itemsInEquipement.boots.loot_id"
+             :id="getTypeByLootTable(itemsInEquipement.boots.loot_id) + itemsInEquipement.boots.item_id"
+             :src="getImageByLootTable(itemsInEquipement.boots.loot_id)" width="100" height="100"
+             data-type="boots">
+      </div>
+      <div class="item-slot" id="secondary-magic-item-slot" @mouseover="openTooltip($event, itemsInEquipement.secondary_magic_item)">
         <img v-if="itemsInEquipement.secondary_magic_item && itemsInEquipement.secondary_magic_item.loot_id"
              :id="getTypeByLootTable(itemsInEquipement.secondary_magic_item.loot_id) + itemsInEquipement.secondary_magic_item.item_id"
              :src="getImageByLootTable(itemsInEquipement.secondary_magic_item.loot_id)" width="100" height="100"
@@ -44,12 +59,49 @@
       <div v-for="index in bag.length"
            class="item-slot mx-2"
            :key="index"
+           @mouseover="openTooltip($event, itemsInBag[index - 1])"
            :id="'bag' + index">
         <img v-if="itemsInBag[index - 1] && itemsInBag[index - 1].loot_id"
              :id="getTypeByLootTable(itemsInBag[index - 1].loot_id) + itemsInBag[index - 1].item_id"
              :src="getImageByLootTable(itemsInBag[index - 1].loot_id)" width="100" height="100"
              :data-type="'bag' + index">
       </div>
+    </div>
+    <div id="item-info-tooltip" class="item-info-tooltip-container" v-show="itemTooltip.isShow">
+      <b-list-group v-if="itemTooltip.item">
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'star']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.level}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'dumbbell']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.strength}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'brain']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.intelligence}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'feather']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.speed}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'user']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.charisma}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'heart']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.health}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'dice']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.luck}}</span>
+        </b-list-group-item>
+        <b-list-group-item v-if="itemTooltip.item.charm">
+          <font-awesome-icon :icon="['fas', 'bolt']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.charm_type}} - {{itemTooltip.item.charm_value}}</span>
+        </b-list-group-item>
+      </b-list-group>
     </div>
   </div>
 </template>
@@ -92,6 +144,10 @@ export default {
         secondary_magic_item: null
       },
       lootTable : [],
+      itemTooltip: {
+        isShow: false,
+        item: null,
+      },
     }
   },
   async beforeMount() {
@@ -150,6 +206,17 @@ export default {
       const loot = await this.$axios.$get(ApiUrls.API_LOOT_TABLE() + '/' + item.loot_id);
       this.lootTable.push(loot.data[0]);
     },
+    openTooltip(event, item) {
+      this.itemTooltip.isShow = true;
+      this.itemTooltip.item = item;
+      const itemInfoTooltip = document.getElementById('item-info-tooltip');
+      itemInfoTooltip.style.left = (event.clientX + 5) + 'px';
+      itemInfoTooltip.style.top = (event.clientY + 5) + 'px';
+      console.log('openTooltip', this.itemTooltip);
+    },
+    closeTooltip() {
+      this.itemTooltip.isShow = false;
+    }
   }
 }
 </script>
@@ -171,5 +238,14 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.item-info-tooltip-container {
+  position: absolute;
+  display: block;
+  z-index: 999;
+  & .list-group-item {
+    font-size: 10px;
+    padding: 5px 1.25rem;
+  }
 }
 </style>
--- pages/game/_id.vue
+++ pages/game/_id.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-100 p-5 row" @click="hideContextMenu()">
+  <div class="w-100 p-5 row" @click="hideContextMenu(); closeTooltip()">
     <div class="col-3 d-flex flex-wrap justify-content-center">
-      <div class="item-slot" id="helmet-slot">
+      <div class="item-slot" id="helmet-slot" @mouseover="openTooltip($event, itemsInEquipement.helmet)">
         <img v-if="itemsInEquipement.helmet && itemsInEquipement.helmet.loot_id"
              @contextmenu="showContextMenu($event)"
              @mouseover="setHoverItem(itemsInEquipement.helmet)"
@@ -9,7 +9,7 @@
              :src="getImageByLootTable(itemsInEquipement.helmet.loot_id)" width="100" height="100"
              data-type="helmet">
       </div>
-      <div class="item-slot" id="chestplate-slot">
+      <div class="item-slot" id="chestplate-slot" @mouseover="openTooltip($event, itemsInEquipement.chestplate)">
         <img v-if="itemsInEquipement.chestplate && itemsInEquipement.chestplate.loot_id"
              @contextmenu="showContextMenu($event)"
              @mouseover="setHoverItem(itemsInEquipement.chestplate)"
@@ -17,7 +17,7 @@
              :src="getImageByLootTable(itemsInEquipement.chestplate.loot_id)" width="100" height="100"
              data-type="chestplate">
       </div>
-      <div class="item-slot" id="primary-magic-item-slot">
+      <div class="item-slot" id="primary-magic-item-slot" @mouseover="openTooltip($event, itemsInEquipement.primary_magic_item)">
         <img v-if="itemsInEquipement.primary_magic_item && itemsInEquipement.primary_magic_item.loot_id"
              @contextmenu="showContextMenu($event)"
              @mouseover="setHoverItem(itemsInEquipement.primary_magic_item)"
@@ -28,7 +28,7 @@
     </div>
     <div class="col-6 d-flex flex-wrap justify-content-center">
       <div class="img-caracter" :style="{ backgroundImage: `url(${caracter.picture})`}"></div>
-      <div class="item-slot mx-2" id="primary-weapon-slot">
+      <div class="item-slot mx-2" id="primary-weapon-slot" @mouseover="openTooltip($event, itemsInEquipement.primary_weapon)">
         <img v-if="itemsInEquipement.primary_weapon && itemsInEquipement.primary_weapon.loot_id"
              @contextmenu="showContextMenu($event)"
              @mouseover="setHoverItem(itemsInEquipement.primary_weapon)"
@@ -36,12 +36,33 @@
              :src="getImageByLootTable(itemsInEquipement.primary_weapon.loot_id)" width="100" height="100"
              data-type="primary_weapon">
       </div>
-      <div class="item-slot mx-2" id="secondary-weapon-slot"></div>
+      <div class="item-slot mx-2" id="secondary-weapon-slot" @mouseover="openTooltip($event, itemsInEquipement.secondary_weapon)">
+        <img v-if="itemsInEquipement.secondary_weapon && itemsInEquipement.secondary_weapon.loot_id"
+             @contextmenu="showContextMenu($event)"
+             @mouseover="setHoverItem(itemsInEquipement.secondary_weapon)"
+             :id="getTypeByLootTable(itemsInEquipement.secondary_weapon.loot_id) + itemsInEquipement.secondary_weapon.item_id"
+             :src="getImageByLootTable(itemsInEquipement.secondary_weapon.loot_id)" width="100" height="100"
+             data-type="secondary_weapon">
+      </div>
     </div>
     <div class="col-3 d-flex flex-wrap justify-content-center">
-      <div class="item-slot" id="gloves-slot"></div>
-      <div class="item-slot" id="boots-slot"></div>
-      <div class="item-slot" id="secondary-magic-item-slot">
+      <div class="item-slot" id="gloves-slot" @mouseover="openTooltip($event, itemsInEquipement.gloves)">
+        <img v-if="itemsInEquipement.gloves && itemsInEquipement.gloves.loot_id"
+             @contextmenu="showContextMenu($event)"
+             @mouseover="setHoverItem(itemsInEquipement.gloves)"
+             :id="getTypeByLootTable(itemsInEquipement.gloves.loot_id) + itemsInEquipement.gloves.item_id"
+             :src="getImageByLootTable(itemsInEquipement.gloves.loot_id)" width="100" height="100"
+             data-type="gloves">
+      </div>
+      <div class="item-slot" id="boots-slot" @mouseover="openTooltip($event, itemsInEquipement.boots)">
+        <img v-if="itemsInEquipement.boots && itemsInEquipement.boots.loot_id"
+             @contextmenu="showContextMenu($event)"
+             @mouseover="setHoverItem(itemsInEquipement.boots)"
+             :id="getTypeByLootTable(itemsInEquipement.boots.loot_id) + itemsInEquipement.boots.item_id"
+             :src="getImageByLootTable(itemsInEquipement.boots.loot_id)" width="100" height="100"
+             data-type="boots">
+      </div>
+      <div class="item-slot" id="secondary-magic-item-slot" @mouseover="openTooltip($event, itemsInEquipement.secondary_magic_item)">
         <img v-if="itemsInEquipement.secondary_magic_item && itemsInEquipement.secondary_magic_item.loot_id"
              @contextmenu="showContextMenu($event)"
              @mouseover="setHoverItem(itemsInEquipement.secondary_magic_item)"
@@ -54,6 +75,7 @@
       <div v-for="index in bag.length"
            class="item-slot mx-2"
            :key="index"
+           @mouseover="openTooltip($event, itemsInBag[index - 1])"
            :id="'bag' + index">
         <img v-if="itemsInBag[index - 1] && itemsInBag[index - 1].loot_id"
              @contextmenu="showContextMenu($event)"
@@ -76,6 +98,42 @@
            @click="useItemFromEquipement(menuInfo.selectedItem)">
         <span>Ranger</span>
       </b-button>
+    </div>
+    <div id="item-info-tooltip" class="item-info-tooltip-container" v-show="itemTooltip.isShow && !menuInfo.isShow">
+      <b-list-group v-if="itemTooltip.item">
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'star']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.level}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'dumbbell']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.strength}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'brain']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.intelligence}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'feather']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.speed}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'user']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.charisma}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'heart']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.health}}</span>
+        </b-list-group-item>
+        <b-list-group-item>
+          <font-awesome-icon :icon="['fas', 'dice']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.luck}}</span>
+        </b-list-group-item>
+        <b-list-group-item v-if="itemTooltip.item.charm">
+          <font-awesome-icon :icon="['fas', 'bolt']" class="ml-2"></font-awesome-icon>
+          <span class="ml-2">{{itemTooltip.item.charm_type}} - {{itemTooltip.item.charm_value}}</span>
+        </b-list-group-item>
+    </b-list-group>
     </div>
   </div>
 </template>
@@ -126,6 +184,10 @@ export default {
         origin: null,
         posX: 0,
         posY: 0
+      },
+      itemTooltip: {
+        isShow: false,
+        item: null,
       }
     }
   },
@@ -187,7 +249,6 @@ export default {
     },
     setHoverItem(item) {
       this.menuInfo.preselectedItem = item;
-      console.log('setHoverItem', this.menuInfo.preselectedItem);
     },
     showContextMenu(event) {
       event.preventDefault();
@@ -276,6 +337,17 @@ export default {
         await this.$axios.$put(ApiUrls.API_ITEMS() + '/' + item.item_id, item);
         console.log('useItemFromEquipement', this.equipement, this.itemsInBag);
       }
+    },
+    openTooltip(event, item) {
+      this.itemTooltip.isShow = true;
+      this.itemTooltip.item = item;
+      const itemInfoTooltip = document.getElementById('item-info-tooltip');
+      itemInfoTooltip.style.left = (event.clientX + 5) + 'px';
+      itemInfoTooltip.style.top = (event.clientY + 5) + 'px';
+      console.log('openTooltip', this.itemTooltip);
+    },
+    closeTooltip() {
+      this.itemTooltip.isShow = false;
     }
   }
 }
@@ -322,5 +394,14 @@ export default {
   }
   .context-menu-item:disabled {
     background-color: gray;
+  }
+  .item-info-tooltip-container {
+    position: absolute;
+    display: block;
+    z-index: 999;
+    & .list-group-item {
+      font-size: 10px;
+      padding: 5px 1.25rem;
+    }
   }
 </style>
